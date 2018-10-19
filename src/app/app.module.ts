8529436bb0ebed9f6e808681d12e9d9a721fb9d9
pawan2726 <pawan152000@gmail.com>
2018-10-19 18:51:31 +0530

    Fix the routing nd implement sample ag-grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,36 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
+import { AgGridModule } from 'ag-grid-angular';
 import { UploadimagesComponent } from './UploadImages/uploadimages.component';
 import {UploaddetailsService} from './Services/uploaddetails.service';
+import { ContactComponent } from './grid/contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: 'upload',
     component: UploadimagesComponent
-  }
+  },
+  { path: 'grid', component: ContactComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '',redirectTo: 'home',pathMatch: 'full'}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    UploadimagesComponent
+    UploadimagesComponent,
+    ContactComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule ,
+    AgGridModule.withComponents([]),
     RouterModule.forRoot(routes)
   ],
   providers: [UploaddetailsService],
