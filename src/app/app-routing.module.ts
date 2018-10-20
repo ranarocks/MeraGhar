cc4e2181e101bc44dfa8005c78aa04f14aee0f58
ranarocks <jatinrana1988@gmail.com>
2018-10-20 22:02:06 +0530

    change in routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,24 @@ import { CommonModule } from '@angular/common';
 import {RouterModule,Routes} from '@angular/router';
 import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
 import { AppComponent } from './app.component';
-import { UploadimagesComponent } from './uploadimages/uploadimages.component';
+import { UploadimagesComponent } from './UploadImages/uploadimages.component';
+import { UploaddetailsService } from './Services/uploaddetails.service';
+import { ContactComponent } from './grid/contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import { AuthGuard } from './auth.guard'
 
 
-export const AppRoutes: Routes =[
-  { path: 'Upload', component: UploadimagesComponent }
-  // { path: 'aregisteration', component: AregistrationComponent },
- 
-
+export const routes: Routes = [
+  {
+    path: 'upload',
+    component: UploadimagesComponent
+  },
+  { path: 'grid', component: ContactComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
-export const ROUTING: ModuleWithProviders = RouterModule.forRoot(AppRoutes);
+export const ROUTING: ModuleWithProviders = RouterModule.forRoot(routes);
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { ROUTING } from "./app-routing.module";
 import { AppComponent } from './app.component';
 
 import { AgGridModule } from 'ag-grid-angular';
-import { UploadimagesComponent } from './UploadImages/uploadimages.component';
-import {UploaddetailsService} from './Services/uploaddetails.service';
-import { ContactComponent } from './grid/contact/contact.component';
-import { HeaderComponent } from './header/header.component';
+  import { UploadimagesComponent } from './UploadImages/uploadimages.component';
+  import {UploaddetailsService} from './Services/uploaddetails.service';
+  import { ContactComponent } from './grid/contact/contact.component';
+  import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [
-  {
-    path: 'upload',
-    component: UploadimagesComponent
-  },
-  { path: 'grid', component: ContactComponent },
-  { path: 'home', component: HomeComponent },
-  { path: '',redirectTo: 'home',pathMatch: 'full'}
-];
+
+//const routes: Routes = [
+//  {
+//    path: 'upload',
+//    component: UploadimagesComponent
+//  },
+//  { path: 'grid', component: ContactComponent },
+//  { path: 'home', component: HomeComponent },
+//  { path: '',redirectTo: 'home',pathMatch: 'full'}
+//];
 
 @NgModule({
   declarations: [
@@ -35,7 +37,7 @@ const routes: Routes = [
     FormsModule,
     HttpClientModule ,
     AgGridModule.withComponents([]),
-    RouterModule.forRoot(routes)
+    ROUTING
   ],
   providers: [UploaddetailsService],
   bootstrap: [AppComponent]
